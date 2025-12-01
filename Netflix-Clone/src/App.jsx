7504e1a2862bc7268cfccbb920336b30e2a61997
Medi Mule <medhanitmulatu6796@gmail.com>
2025-12-02 00:48:10 +0300

    Netflix Header and Footer added successfully

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className='App'>
-      App
-    </div>
+   <div className='App'>
+   <Home />
+   </div>
   )
 }
 
